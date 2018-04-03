a32277f8915b03e2811ec8f321e13aa51d53b3dd
Ron Smits <ron.smits@gmail.com>
2018-04-03 09:02:23 +0200

    first commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,28 @@
+package com.example.demo
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import java.util.*
+
+@SpringBootApplication
+class DemoApplication {
+    @Bean
+    fun commandLineRunner(ctx: ApplicationContext)=CommandLineRunner {
+
+
+        println("Let's inspect the beans provided by Spring Boot:")
+
+        ctx.beanDefinitionNames.sorted().forEach(::println)
+
+
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+
---
+++ src/main/kotlin/com/example/demo/model/Person.kt
@@ -0,0 +1,15 @@
+package com.example.demo.model
+
+import org.springframework.data.repository.CrudRepository
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class Person (val firstName: String, val lastName : String){
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id : Int = 0
+
+}
+
+interface PersonRepo : CrudRepository<Person, Int>
